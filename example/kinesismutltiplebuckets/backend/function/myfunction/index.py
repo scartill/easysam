e28b301b4a13a9aa01f3623d26e6b93e3a6aa1d1
Boris Resnick <boris.resnick@gmail.com>
2025-07-23 10:35:05 +0300

    feat(kinesis): add dual stream publishing to function

--- example/kinesismutltiplebuckets/backend/function/myfunction/index.py
+++ example/kinesismutltiplebuckets/backend/function/myfunction/index.py
@@ -1,7 +1,27 @@
+import os
 import json
+
+import boto3
 
 
 def handler(event, context):
+    message = json.loads(event['body'])
+    print('Message received: ', message)
+    env = os.getenv('ENV')
+    simple_stream_name = f'simple-{env}'
+    complex_stream_name = f'complex-{env}'
+    kinesis = boto3.client('kinesis')
+
+    kinesis.put_record(
+        StreamName=complex_stream_name,
+        Data=json.dumps(message),
+    )
+
+    kinesis.put_record(
+        StreamName=simple_stream_name,
+        Data=json.dumps(message),
+    )
+
     return {
         'statusCode': 200,
         'headers': {
@@ -9,5 +29,5 @@ def handler(event, context):
             'Access-Control-Allow-Origin': '*',
             'Access-Control-Allow-Methods': '*'
         },
-        'body': json.dumps({'data': 'some data'})
+        'body': json.dumps({'Result': 'OK'})
     }
