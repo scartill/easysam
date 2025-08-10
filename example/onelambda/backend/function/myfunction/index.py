bfe1f7ef101e9ba50b13df285f46b785ab951c29
Boris Resnick <boris.resnick@gmail.com>
2025-08-10 20:14:37 +0300

    feat(customlayers): Example stub

---
+++ example/onelambda/backend/function/myfunction/index.py
@@ -0,0 +1,13 @@
+import json
+
+
+def handler(event, context):
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Access-Control-Allow-Headers': '*',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': '*'
+        },
+        'body': json.dumps({'data': 'Hello, World!'})
+    }
