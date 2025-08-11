4b02d851f9430ff7fc512571c9524f7609d96051
Boris Resnick <boris.resnick@gmail.com>
2025-08-11 15:47:10 +0300

    feat: add custom layer validation with SSM parameter support

--- src/easysam/validate_cloud.py
+++ src/easysam/validate_cloud.py
@@ -15,7 +15,11 @@ def validate(cliparams: dict, resources_data: dict, environment: str, errors: li
     '''
 
     iam = get_aws_client('iam', cliparams)
+    ssm = get_aws_client('ssm', cliparams)
+    lambdas = get_aws_client('lambda', cliparams)
+
     validate_bucket_policy(iam, resources_data, environment, errors)
+    validate_custom_layers(ssm, lambdas, resources_data, errors)
 
 
 def validate_bucket_policy(iam, resources_data, environment, errors):
@@ -47,3 +51,46 @@ def validate_bucket_policy(iam, resources_data, environment, errors):
                     f"Bucket '{bucket}' has an invalid extaccesspolicy: {policy_name}. "
                     f"Please create a policy with the name {full_policy_name}."
                 )
+
+
+def validate_custom_layers(ssm, lambdas, resources_data, errors):
+    for function, details in resources_data.get('functions', {}).items():
+        layers = details.get('layers', {})
+
+        for layer, layer_handle in layers.items():
+            if layer_handle.startswith('{{resolve:'):
+                param_uri = layer_handle.split('resolve:')[1].split('}}')[0]
+
+                if not param_uri.startswith('ssm:'):
+                    errors.append(
+                        f'Custom layer {layer} by URI in ({function}) is not yet supported'
+                    )
+                    continue
+
+                ssm_param = param_uri.split('ssm:')[1]
+                ssm_param = ssm_param.lstrip('/')
+                lg.info(f'Validating SSM layer name: {ssm_param}')
+
+                try:
+                    layer_handle = ssm.get_parameter(Name=ssm_param)['Parameter']['Value']
+                    lg.info(f'Successfully resolved SSM layer name: {ssm_param}')
+                    lg.info(f'Layer ARN received: {layer_handle}')
+
+                except Exception:
+                    errors.append(f'SSM parameter {ssm_param} not found')
+                    continue
+
+            if layer_handle.startswith('arn:'):
+                lg.info(f'Looking for layer ARN: {layer_handle}')
+
+                try:
+                    response = lambdas.get_layer_version_by_arn(Arn=layer_handle)
+                    version = response['Version']
+                    lg.info(f'Layer ARN found: {layer_handle}, version: {version}')
+                    continue
+
+                except Exception:
+                    errors.append(f'Layer ARN {layer_handle} not found')
+                    continue
+
+            errors.append(f"Custom layer {layer} in function {function} is not supported")
