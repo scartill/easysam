b6472d189b98c8eff7d0fab2e8f38488d0e059d7
Boris Resnick <boris.resnick@gmail.com>
2025-11-25 01:22:23 +0200

    example: enable TTL support for DynamoDB model with ExpireAt field

--- example/prismarinettl/common/myobject/models.py
+++ example/prismarinettl/common/myobject/models.py
@@ -5,8 +5,9 @@ from prismarine.runtime import Cluster
 c = Cluster('PrismaTTL')
 
 
-@c.model(PK='Foo', SK='Bar')  # , ttl='ExpireAt')
+@c.model(PK='Foo', SK='Bar', ttl='ExpireAt')
 class Item(TypedDict):
     Foo: str
     Bar: str
     Baz: NotRequired[str]
+    ExpireAt: int
