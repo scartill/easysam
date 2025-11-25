a6e05755974139f1e3be62d66a7728140fb7ae84
Boris Resnick <boris.resnick@gmail.com>
2025-11-25 12:22:17 +0200

    feat: add NestedItem model with nested object support

--- example/prismapydantic/common/myobject/models.py
+++ example/prismapydantic/common/myobject/models.py
@@ -5,8 +5,14 @@ from pydantic import BaseModel, Field
 c = Cluster('PrismaPydantic')
 
 
+@c.export
+class NestedItem(BaseModel):
+    NestedFoo: str = Field(description="The nested foo attribute")
+
+
 @c.model(PK='Foo', SK='Bar')
 class Item(BaseModel):
     Foo: str = Field(description="The Foo attribute")
     Bar: str = Field(description="The Bar attribute")
     Baz: str = Field(description="The Baz attribute")
+    Nested: NestedItem = Field(description="The Nested attribute")
