92e8780967fd51fd2b696a3c14d3938896f4c45a
Boris Resnick <boris.resnick@gmail.com>
2025-11-25 13:25:39 +0200

    style(models): change field descriptions from double to single quotes

--- example/prismapydantic/common/myobject/models.py
+++ example/prismapydantic/common/myobject/models.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from prismarine.runtime import Cluster
 from pydantic import BaseModel, Field
 
@@ -7,12 +8,12 @@ c = Cluster('PrismaPydantic')
 
 @c.export
 class NestedItem(BaseModel):
-    NestedFoo: str = Field(description="The nested foo attribute")
+    NestedFoo: str = Field(description='The nested foo attribute')
 
 
 @c.model(PK='Foo', SK='Bar')
 class Item(BaseModel):
-    Foo: str = Field(description="The Foo attribute")
-    Bar: str = Field(description="The Bar attribute")
-    Baz: str = Field(description="The Baz attribute")
-    Nested: NestedItem = Field(description="The Nested attribute")
+    Foo: str = Field(description='The Foo attribute')
+    Bar: str = Field(description='The Bar attribute')
+    Baz: Optional[str] = Field(description='The Baz attribute')
+    Nested: NestedItem = Field(description='The Nested attribute')
